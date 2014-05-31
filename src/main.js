3f732b130129dbbadf76232b323e10f8445a12f5
Bryon Vandiver <bryon@crowdtilt.com>
2014-05-31 14:56:30 -0700

    Checkpoint

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-var tmpl = require("./views/main.ejs");
+var instr = require("./cpu/instructions.json"),
+	tmpl = require("./views/main.ejs");
+
+console.log(instr[0])
 
 Array.prototype.forEach.call(document.querySelectorAll("antic"), function (el) {
 	el.innerHTML = tmpl({});
